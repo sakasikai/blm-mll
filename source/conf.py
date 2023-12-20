6e7b4c2b5c56835a8d99d8ba71368e65c76fe910
zhangxiaoqi <tonq_dlut@126.com>
2023-12-20 15:45:53 +0800

    edit

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
 
 
